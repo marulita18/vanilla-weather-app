0520a739cc43ac1d5250f62ce2b15b6c7f8f7d2f
marulita18 <82970308+marulita18@users.noreply.github.com>
2021-04-28 21:15:00 +0200

    Injected weather forecast with JS

--- src/index.js
+++ src/index.js
@@ -13,7 +13,34 @@ let day = days[date.getDay()];
 
 return `${day} ${hours}:${minutes}`;
 }
-
+ 
+function displayForecast (){
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class="row">`;
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    days.forEach(function (day) {
+        forecastHTML =
+        forecastHTML +
+        `  
+        <div class="col-2">
+          <div class="weather-forecast-date">
+          ${day}
+        </div>
+          <img src="http://openweathermap.org/img/wn/02d@2x.png"
+          alt="sunny"
+          width="40" 
+          id="weather-forecast-icon"
+          /> 
+          <div class="weather-forecast-temperatures"> 
+            <span class="weather-forecast-temperature-min">11°</span>
+            <span class="weather-forecast-temperature-max">14°</span> 
+        </div>
+        </div>
+        `;
+    });
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
 
 
 function showTemperature(response) {
@@ -64,6 +91,7 @@ temperatureElement.innerHTML = Math.round(celsiusTemperature);
 
 let celsiusTemperature = null;
 search("Amsterdam");
+displayForecast();
 
 
 let form = document.querySelector("#search-form");
